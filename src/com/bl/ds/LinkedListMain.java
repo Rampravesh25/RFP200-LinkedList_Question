4a680049a07de733a91bee6598d50c739072b4fb
Rampravesh25 <rampraveshmallah@gmail.com>
2022-09-28 08:05:13 +0530

    Added:Inserting the element

--- src/com/bl/ds/LinkedListMain.java
+++ src/com/bl/ds/LinkedListMain.java
@@ -18,5 +18,13 @@ public class LinkedListMain {
 
         System.out.println();
         l2.print();
+
+        LinkedList l3 = new LinkedList();
+        l3.add(56);
+        l3.add(30);
+
+        l3.insert(70,56);
+        System.out.println();
+        l3.print();
     }
 }
